f5e289a6ca898c7e9b2ec6692a3fe3d067b30a46
v-fangyt2 <liubomo@yeah.net>
2025-05-28 15:48:04 +0800

    feat：地图页面开发

---
+++ src/views/map/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="myMap"></div>
+</template>
+
+<script>
+expport default {
+  mounted() {
+    this.map = new T.Map('my-TDMap', {
+            projection: 'EPSG:4326',
+            minZoom: 7,
+            maxZoom: 18,
+            zoom: 15,
+            center: new T.LngLat(113.31788, 23.10674)
+        })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
